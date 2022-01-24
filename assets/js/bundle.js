415355dbd9e5658876ffd780c53a2d7ccb17a2ef
gustavokuklinski <ghc.kuklinski@gmail.com>
2022-01-24 17:30:14 -0300

    Removido Web3 para Faucet

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -3,7 +3,8 @@ const tokenSymbol = 'ART$';
 const tokenDecimals = 18;
 const tokenImage = 'https://bink.binaria.art.br/art-symbol.png';
 
-const dropAddress = "0x3d90c6667d767020fa0a75dd77f763ce5d806556";
+const dropAddress = "0x3d90c6667d767020fa0a75dd77f763ce5d806556"; // Mainnet
+//const dropAddress = "0xbb9aea25534745d8231d977262e41edcc38bba6b"; // Testnet
 
 // Add Bink (ART$) to Wallet list
 async function addBinkMatic() {
@@ -31,15 +32,22 @@ async function addBinkMatic() {
   }
 }
 async function faucetBinkMatic() {
-  let faucetABI = [
-    {"inputs":[{"internalType":"address","name":"_smtAddress","type":"address"},{"internalType":"address","name":"_ownerAddress","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"send","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"setFaucetDripAmount","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_tokenAddr","type":"address"}],"name":"setTokenAddress","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"_receiver","type":"address"},{"internalType":"uint256","name":"_amount","type":"uint256"}],"name":"withdrawTokens","outputs":[],"stateMutability":"nonpayable","type":"function"}
-  ]
-  if (window.ethereum) {
-    window.web3 = new Web3(window.ethereum);
-    const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
-    const account = accounts[0];
-    let walletAddress = account;
-    let contractDropArtsTo = new web3.eth.Contract(faucetABI, dropAddress);  
-    contractDropArtsTo.methods.send().send({ from: walletAddress }).then(console.log);
-  }
+
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+  const account = accounts[0];
+
+  const faucetParams = {
+    from: account, 
+    to: dropAddress,
+    data: '0xb46300ec',
+    chainId: '0x89', 
+    
+  };
+
+  const faucetTxHash = await ethereum.request({
+    method: 'eth_sendTransaction',
+    params: [faucetParams],
+  });
+
+  console.log(faucetTxHash);
 }
